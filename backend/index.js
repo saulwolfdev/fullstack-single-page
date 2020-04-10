ed61ab97968f68c77f6de3d673230766e1f244ac
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-04-10 16:13:34 -0300

    servidor configurado middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
 const express=require("express");
-
+const morgan=require("morgan");
+const multer=require('multer');
+const path=require("path")
 //initialization
 const app =express();
 
 //settings
 
 app.set("port",3000);
+//middlewares
+
+app.use(morgan("dev"));
+const storage=multer.diskStorage({
+  destination: path.join(__dirname, "public/uploads"),
+      filename(res,file,cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+      }
+});
+
+app.use(multer({storage}).single("image"));
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json())
 
 //start the server
 
