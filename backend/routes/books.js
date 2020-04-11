5670f2eff355bcdf7ca8dadc359e8fc476202ba8
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-04-11 13:52:14 -0300

    backend completo, rest-api, get,post,delete

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -9,6 +9,18 @@ router.get("/",async(req,res)=>{
     const books=await Book.find()
     res.json(books)
 })
-
+router.post("/",async(req,res)=>{
+    console.log(req.body)
+    const {title,author,isbn}=req.body
+    const newBook=new Book({title,author,isbn})
+     await newBook.save()
+    res.json({message:"Book Saved"})
+})
+router.delete("/:id",async(req,res)=>{
+     await Book.findByIdAndDelete(req.params.id);
+    res.json({
+        message:"this is book has deleted"
+    })
+})
 
  module.exports=router;
